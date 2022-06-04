d983c73336231364cc8cbd13727bbc61aba549e4
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-04 14:00:52 +0300

    done assignement

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+  }
+  const gifts = ["teddy bear", "drone", "doll"];
+
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+     
+    }
+  
+    return gifts;
+  }
+  
+  
+  wrapGifts(gifts);
+  const cards = [ 'Guadalupe', 'Ollie', 'Aki' ] ;
+  function writeCards(cards){
+      for (let i = 0; i < cards.length; i++){
+          console.log(`Thank you, ${cards[i]} for the wonderful Christmas gift!`)
+      }
+    return cards;
+  }
+  writeCards(cards);
+  function countDown(i) {
+    let countDown = i;
+    while (countDown >= 0) {
+      console.log(countDown--);
+    }}
+    countDown(10)
